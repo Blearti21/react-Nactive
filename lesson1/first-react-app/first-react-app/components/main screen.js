30d9ff193155f1e305cd3abfc95718a0b944f33e
Blearti21 <bleartvitia91@gmail.com>
2025-11-06 20:44:30 +0100

    day 2

---
+++ lesson1/first-react-app/first-react-app/components/main screen.js
@@ -0,0 +1,12 @@
+import Reach from "react";
+import {Text, StyleSheet, View} from "react-native"
+
+function MainScreen(){
+    return(
+        <>
+        <div>Hello from main screen</div>
+        </>
+    )
+
+}
+export default MainScreen
