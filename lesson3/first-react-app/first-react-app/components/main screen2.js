87fbec6201376b1340509ca29c078ac8ae48f46f
Blearti21 <bleartvitia91@gmail.com>
2025-11-11 20:43:22 +0100

    day3

---
+++ lesson3/first-react-app/first-react-app/components/main screen2.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Text, StyleSheet, View} from "react-native"
+
+
+const MainScreen=()=>{
+
+    return(
+        <View style={}>
+            <Text style={style.textStyle} > This is main screen 2</Text>
+
+        </View>
+    );
+};
+
+const style = StyleSheet.create({
+
+    container={
+        flex=1;
+        backgroundColor: "#9c4c4cff",
+        alignItems:"center"
+    },
+
+    textStyle:{
+        fontSize: 30,
+    },
+
+
+
+
+})
+
+export default MainScreen2
