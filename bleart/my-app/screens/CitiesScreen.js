d41a8d6c74ba0b86d6c201928c5722d1cc887d2e
Blearti21 <bleartvitia91@gmail.com>
2026-01-13 20:46:51 +0100

    day16

---
+++ bleart/my-app/screens/CitiesScreen.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+import { StyleSheet,View, Text, FlatList } from "react-native";
+import data from "../data/cities.json";
+
+class CitiesScreen extends Comment {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cities: [],
+    }
+}
+
+componentdidMount(){
+    this.setState({cities: data})
+}
+renderItem = ({item})=>{
+    const{name,countryCode,population,description}=item;
+    return(
+        <View style={styles.cardWrapper}>
+            <Text>style={styles.title}{name}</Text>
+            <Text>style={styles.subtitle}County Code:{countryCode}</Text>
+            <Text>style={styles.description}{description}</Text>
+            <Text>style={styles.small}{name}</Text>
+        </View>
+    );
+} ;
+render(){
+    return(
+        <View>
+            <Text style={styles.screenTitle}>Cities</Text>
+
+            <FlatList
+            data={this.state.cities}
+            keyExtractor={(item,index)=>
+                item.id ? item.id.toString() :index.toString()
+            }
+        renderItem={this.renderItem}
+        ></FlatList>
+        </View>
+    )
+}
+}
+
+export default CitiesScreen;
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        padding:16,
+    },
+    screenTitle:{
+        fontsize:22,
+        fontWeight:"bold",
+        marginBottom:12,
+        textAlign:"center"
+    },
+    cardWrapper:{
+        backgroundColor:"#f9f9f9",
+        padding:12,
+        marginBottom:10,
+        borderRadius:8
+    },
+    title:{
+        fontsize:14,
+        fontWeight:"bold"
+    },
+    subtitle:{
+        fontSize:14,
+        marginTop:6,
+        color:"#555"
+    },
+    description:{
+        fontSize:13,
+        marginTop:6,
+        color:"#555"
+    },
+    small:{
+        marginTop:6,
+        fontSize:12,
+        color:"#777"
+    }
+})
+
+
+
+
+
